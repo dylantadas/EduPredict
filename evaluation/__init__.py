49718d89497d8eead7ee8638bf41719d80346418
Dylan <dtadas@iu.edu>
2025-03-25 14:40:18 -0400

    Reverted init files, deleted setup_path.py file, modified imports in dev_notebook.ipynb

--- evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-# re-export functions from performance_metrics.py
-from .performance_metrics import (
-    analyze_feature_importance,
-    analyze_feature_correlations,
-    calculate_model_metrics,
-    calculate_fairness_metrics,
-    plot_roc_curves,
-    plot_fairness_metrics
-)
