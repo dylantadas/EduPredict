6ec13a9219e9f7c2ba8ea6e9bd6b0bb4bc4053e1
Dylan <dtadas@iu.edu>
2025-03-25 14:40:18 -0400

    Reverted init files, deleted setup_path.py file, modified imports in dev_notebook.ipynb

--- data_analysis/__init__.py
+++
@@ -1,8 +0,0 @@
-# re-export functions from eda.py
-from .eda import (
-    perform_automated_eda,
-    analyze_student_performance,
-    analyze_engagement_patterns,
-    document_eda_findings,
-    visualize_demographic_distributions
-)
--- model_training/__init__.py
+++
@@ -1,5 +0,0 @@
-# re-export functions from hyperparameter_tuning.py
-from .hyperparameter_tuning import (
-    tune_random_forest,
-    visualize_tuning_results
-)
